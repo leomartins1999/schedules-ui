a3381574e4189a6c33a530d27f641c135848aea9
leomartins1999 <36698491+leomartins1999@users.noreply.github.com>
2021-11-01 19:08:54 +0000

    Add boilerplate setup for app (#2)
    * Add boostrap dependencies
    
    * Update docker-compose to include auto-reload
    
    * Remove boilerplate files; Add boilerplate code for app

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,20 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+function NavigationBar() {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">MySchedule</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#schedules">Schedules</Nav.Link>
+                    <Nav.Link href="#compare">Compare</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
---
+++ src/pages/ComparePage.js
@@ -0,0 +1,9 @@
+function ComparePage() {
+  return (
+    <>
+      <h1>Compare Page!</h1>
+    </>
+  );
+}
+
+export default ComparePage;
