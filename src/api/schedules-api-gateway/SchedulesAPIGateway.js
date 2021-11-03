fe6cb9cdada9e7e7aa4f285f28514b454fc64578
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-11-03 18:16:15 +0000

    Add create schedule operation

--- src/api/schedules-api-gateway/SchedulesAPIGateway.js
+++ src/api/schedules-api-gateway/SchedulesAPIGateway.js
@@ -1,14 +1,39 @@
-const GET_SCHEDULES = process.env.REACT_APP_GET_SCHEDULES_PATH;
+import { readFile } from "../../utils/FileReader";
+
+const SCHEDULES_PATH = process.env.REACT_APP_SCHEDULES_PATH;
 
 async function getSchedules() {
-    const resp = await fetch(GET_SCHEDULES);
+    const resp = await fetch(SCHEDULES_PATH);
     if (!resp.ok) throw Error("Error fetching schedules!");
 
     return await resp.json();
 }
 
+async function createSchedule(name, file) {
+    const options = {
+        method: "POST",
+        body: await buildCreateScheduleBody(name, file)
+    }
+
+    const resp = await fetch(SCHEDULES_PATH, options);
+    if (!resp.ok) throw Error("Error creating schedule!")
+
+    return await resp.json()
+}
+
+async function buildCreateScheduleBody(name, file) {
+    return JSON.stringify(
+        {
+            name: name,
+            format: file.name.split(".").at(-1).toUpperCase(),
+            content: await readFile(file)
+        }
+    )
+}
+
 const SchedulesAPIGateway = {
-    getSchedules
+    getSchedules,
+    createSchedule
 }
 
 export default SchedulesAPIGateway
